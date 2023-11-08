46c21d82c6e3cb5fa9eb1991ca98b04772c3e585
farhan-tkj1 <148602004+farhan-tkj1@users.noreply.github.com>
2023-11-08 22:09:55 +0700

    Add files via upload

---
+++ Tugas_Perulangan/Farhan_Perulangan_1.py
@@ -0,0 +1,14 @@
+#nama: M Farhan Habibi
+#Kelas: XI TKJ 1
+#Absen: 17
+#Soal: Seorang petani memiliki 100 ekor ayam di peternakannya. Setiap bulan, jumlah ayam bertambah 5% dari jumlah ayam pada bulan sebelumnya. Buatlah program yang menghitung berapa bulan yang dibutuhkan agar jumlah ayam melebihi 200 ekor.
+
+ayam = 100
+bulan = 0
+
+while ayam <= 200:
+    bulan += 1
+    pertambahan_ayam = 0.05 * ayam
+    ayam += pertambahan_ayam
+
+print("Jumlah ayam melebihi 200 ekor pada bulan ke-", bulan)
---
+++ Tugas_Perulangan/Farhan_Perulangan_3.py
@@ -0,0 +1,14 @@
+#nama: M Farhan Habibi
+#Kelas: XI TKJ 1
+#Absen: 17
+#Soal: Sebuah investasi awal sebesar 10.000 dollar tumbuh sebesar 6% setiap tahunnya. Buatlah program yang menghitung berapa tahun yang dibutuhkan agar nilai investasi melebihi 20.000 dollar.
+
+investasi = 10000
+tahun = 0
+
+while investasi <= 20000:
+    tahun += 1
+    pertumbuhan_investasi = 0.06 * investasi
+    investasi += pertumbuhan_investasi
+
+print("Dibutuhkan", tahun, "tahun agar nilai investasi melebihi 20,000 dollar.")
